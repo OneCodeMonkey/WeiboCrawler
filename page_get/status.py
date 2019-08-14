e41066e395524d3629c1adeb7625257a28a27198
OneCodeMonkey <1460018362@qq.com>
2019-08-15 00:24:57 +0800

    page_get/status

---
+++ page_get/status.py
@@ -0,0 +1,23 @@
+# -*- coding=UTF-8 -*-
+import json
+from utils import filters
+from page_get.basic import get_page
+
+base_url = "http://weibo.com/p/aj/mblog/getlongtext?ajwvr=6&mid={}"
+
+
+def get_cont_of_weibo(mid):
+    url = base_url.format(mid)
+    html = get_page(url, user_verify=False)
+
+    if html:
+        try:
+            html = json.loads(html, encoding='UTF-8').get('data').get('html')
+            cont = filters.text_filter(html)
+        except AttributeError:
+            cont = ''
+
+        return cont
+
+
+__all__ = ['get_cont_of_weibo']
