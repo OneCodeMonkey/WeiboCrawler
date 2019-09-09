76f5ac355d71c44164cffe6bb370a25aa3e48ec0
OneCodeMonkey <1460018362@qq.com>
2019-09-09 11:25:55 +0800

    test: get_topic_data

--- testGetData.py
+++ testGetData.py
@@ -3,6 +3,6 @@ import requests
 
 def get_topic_data():
     from tasks.topic import search_keyword_topic
-    search_keyword_topic("#香港事件#",33)
+    search_keyword_topic("#一般人驾驭不了的芭比粉#", 33)
 
 get_topic_data()
