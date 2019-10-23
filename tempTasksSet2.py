7d74fbffa4141735b1894e0f89d9b72cd32ce4bb
OneCodeMonkey <1460018362@qq.com>
2019-10-23 20:07:07 +0800

    fix

--- tempTasksSet2.py
+++ tempTasksSet2.py
@@ -1,16 +1,8 @@
 from tasks.user import crawl_person_infos
 from tasks.home import crawl_weibo_datas
 
-# crawl_person_infos(2577139143)
-# crawl_person_infos(6421571119)
-# crawl_person_infos(1895124711)
+crawl_person_infos(2577139143)
 
-# crawl_weibo_datas(2577139143)
-crawl_weibo_datas(6421571119)
-# crawl_weibo_datas(1895124711)
-# crawl_weibo_datas(3099016097)
-# crawl_weibo_datas(1769165581)
-# crawl_weibo_datas(2549228714)
-# crawl_weibo_datas(1713926427)
+crawl_weibo_datas(2577139143)
 
 
