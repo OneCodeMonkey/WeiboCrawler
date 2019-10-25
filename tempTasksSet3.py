0a2db8742ca2e45ec1a69dbe41c730584527c4c9
OneCodeMonkey <1460018362@qq.com>
2019-10-25 14:42:30 +0800

    modify

--- tempTasksSet3.py
+++ tempTasksSet3.py
@@ -2,6 +2,6 @@ from tasks.user import crawl_person_infos
 from tasks.home import crawl_weibo_datas
 
 
-crawl_person_infos(5664459603)
+crawl_person_infos(1783475181)
 
-crawl_weibo_datas(5664459603)
+crawl_weibo_datas(1783475181)
--- tempTasksSet4.py
+++ tempTasksSet4.py
@@ -2,6 +2,6 @@ from tasks.user import crawl_person_infos
 from tasks.home import crawl_weibo_datas
 
 
-crawl_person_infos(2656274875)
+crawl_person_infos(6626867192)
 
-crawl_weibo_datas(2656274875)
+crawl_weibo_datas(6626867192)
