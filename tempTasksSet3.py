838d84d54eea8ffa590a03a7ec70b67aaf85eb2b
OneCodeMonkey <1460018362@qq.com>
2019-10-26 23:22:02 +0800

    remove

--- tempTasksSet3.py
+++
@@ -1,7 +0,0 @@
-from tasks.user import crawl_person_infos
-from tasks.home import crawl_weibo_datas
-
-
-crawl_person_infos(1783475181)
-
-crawl_weibo_datas(1783475181)
--- tempTasksSet4.py
+++
@@ -1,7 +0,0 @@
-from tasks.user import crawl_person_infos
-from tasks.home import crawl_weibo_datas
-
-
-crawl_person_infos(6626867192)
-
-crawl_weibo_datas(6626867192)
