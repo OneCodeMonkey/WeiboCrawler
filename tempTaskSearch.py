2f68be5bef0a5cc0aa00e318fa2e4bb0f55a069d
OneCodeMonkey <1460018362@qq.com>
2019-10-26 21:50:23 +0800

    modify

--- tempTaskSearch.py
+++ tempTaskSearch.py
@@ -4,35 +4,38 @@ import time
 import datetime
 from logger import crawler
 
-STARTTIME = '2019-10-24 21:00:00'
-ENDTIME = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-KEYWORD = '#这辈子嚼过最难忘的东西#'
+topics = [""]
 
-start_time = datetime.datetime.strptime(STARTTIME, "%Y-%m-%d %H:%M:%S")
-end_time = datetime.datetime.strptime(ENDTIME, "%Y-%m-%d %H:%M:%S")
+for topic in topics:
+    STARTTIME = '2019-10-23 11:00:00'
+    ENDTIME = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+    KEYWORD = topic
 
-OneHour = datetime.timedelta(hours=1)
+    start_time = datetime.datetime.strptime(STARTTIME, "%Y-%m-%d %H:%M:%S")
+    end_time = datetime.datetime.strptime(ENDTIME, "%Y-%m-%d %H:%M:%S")
 
-time1 = start_time
-time2 = start_time + OneHour
-
-
-def get_topic_data(keyword, start_time='', end_time=''):
-    from tasks.topic import search_keyword_topic
-    search_keyword_topic(keyword, 33, start_time, end_time)
-
-
-while 1:
-    while time1 < end_time:
-        start_time_str = time1.strftime("%Y-%m-%d-%H")
-        end_time_str = time2.strftime("%Y-%m-%d-%H")
-        print(start_time_str, '--', end_time_str)
-
-        crawler.info("we are crawling keyword:{}, timerange {}:{} content" . format(KEYWORD, start_time_str, end_time_str))
-        get_topic_data(KEYWORD, start_time_str, end_time_str)
-        time1 = time1 + OneHour
-        time2 = time2 + OneHour
+    OneHour = datetime.timedelta(hours=1)
 
     time1 = start_time
     time2 = start_time + OneHour
-    end_time = datetime.datetime.strptime(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())), "%Y-%m-%d %H:%M:%S")
+
+
+    def get_topic_data(keyword, start_time='', end_time=''):
+        from tasks.topic import search_keyword_topic
+        search_keyword_topic(keyword, 33, start_time, end_time)
+
+
+    while 1:
+        while time1 < end_time:
+            start_time_str = time1.strftime("%Y-%m-%d-%H")
+            end_time_str = time2.strftime("%Y-%m-%d-%H")
+            print(start_time_str, '--', end_time_str)
+
+            crawler.info("we are crawling keyword:{}, timerange {}:{} content" . format(KEYWORD, start_time_str, end_time_str))
+            get_topic_data(KEYWORD, start_time_str, end_time_str)
+            time1 = time1 + OneHour
+            time2 = time2 + OneHour
+
+        time1 = start_time
+        time2 = start_time + OneHour
+        end_time = datetime.datetime.strptime(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())), "%Y-%m-%d %H:%M:%S")
