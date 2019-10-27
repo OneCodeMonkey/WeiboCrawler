5337120b220879212ad210ecfd90eb14db7a45e6
OneCodeMonkey <1460018362@qq.com>
2019-10-27 10:26:48 +0800

    fix

--- tempTaskSearch.py
+++ tempTaskSearch.py
@@ -4,7 +4,7 @@ import time
 import datetime
 from logger import crawler
 
-topics = ["#宿舍放布娃娃被通报批评#", "#大连行凶男孩舅舅首发声#", "#大连被害女孩曾写信给父母#", "#67岁孕妇自然受孕产女#", "#大连男孩被曝多次尾随女性#", "#15岁中学生砖头打老师被刑拘#", "#日本天价柿子两个5万元#", "#为何未成年人低龄化犯罪频发#", "#14个越南家庭报案家人疑失踪英国#", "#当撒贝宁遇上康辉#", "#大连遇害女童家属聘请律师#", "#英货车藏尸案已逮捕4人#", "#地铁内使用电子产品不许外放#", "#今天环卫工人节#", "#国家要求减少地铁火车站重复安检#"]
+topics = ["#大连行凶男孩舅舅首发声#", "#大连被害女孩曾写信给父母#", "#67岁孕妇自然受孕产女#", "#大连男孩被曝多次尾随女性#", "#15岁中学生砖头打老师被刑拘#", "#日本天价柿子两个5万元#", "#为何未成年人低龄化犯罪频发#", "#14个越南家庭报案家人疑失踪英国#", "#当撒贝宁遇上康辉#", "#大连遇害女童家属聘请律师#", "#英货车藏尸案已逮捕4人#", "#地铁内使用电子产品不许外放#", "#今天环卫工人节#", "#国家要求减少地铁火车站重复安检#"]
 
 for topic in topics:
     STARTTIME = '2019-10-24 11:00:00'
@@ -25,17 +25,13 @@ for topic in topics:
         search_keyword_topic(keyword, 33, start_time, end_time)
 
 
-    while 1:
-        while time1 < end_time:
-            start_time_str = time1.strftime("%Y-%m-%d-%H")
-            end_time_str = time2.strftime("%Y-%m-%d-%H")
-            print(start_time_str, '--', end_time_str)
 
-            crawler.info("we are crawling keyword:{}, timerange {}:{} content" . format(KEYWORD, start_time_str, end_time_str))
-            get_topic_data(KEYWORD, start_time_str, end_time_str)
-            time1 = time1 + OneHour
-            time2 = time2 + OneHour
+    while time1 < end_time:
+        start_time_str = time1.strftime("%Y-%m-%d-%H")
+        end_time_str = time2.strftime("%Y-%m-%d-%H")
+        print(start_time_str, '--', end_time_str)
 
-        time1 = start_time
-        time2 = start_time + OneHour
-        end_time = datetime.datetime.strptime(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())), "%Y-%m-%d %H:%M:%S")
+        crawler.info("we are crawling keyword:{}, timerange {}:{} content" . format(KEYWORD, start_time_str, end_time_str))
+        get_topic_data(KEYWORD, start_time_str, end_time_str)
+        time1 = time1 + OneHour
+        time2 = time2 + OneHour
