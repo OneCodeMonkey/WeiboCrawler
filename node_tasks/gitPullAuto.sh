27c5d328c73f75bed62796c045aeb90e53f3e433
OneCodeMonkey <1460018362@qq.com>
2019-11-23 11:58:23 +0800

    add bash scripts

---
+++ node_tasks/gitPullAuto.sh
@@ -0,0 +1,1 @@
+cd /home/thefair/www/WeiboCrawler && git checkout user_tag_search_dev && git pull origin user_tag_search_dev
---
+++ node_tasks/startUpTasks.sh
@@ -0,0 +1,1 @@
+cd /home/thefair/www/WeiboCrawler && /usr/local/bin/python3.6 /bin/celery beat -A tasks.workers -l info & && nohup /usr/local/bin/python3.6 /bin/celery -A tasks.workers -Q login_queue,user_crawler,home_crawler,ajax_home_crawler worker -l info -c 1 &
