b0121f586370e322ffc4e6340ca67260d50d18c1
OneCodeMonkey <1460018362@qq.com>
2019-12-03 17:33:05 +0800

    modify

--- testGetData.py
+++ testGetData.py
@@ -1,38 +1,12 @@
-# -*- coding=UTF-8 -*-
-import requests
+from tasks.user import crawl_person_infos
+from tasks.home import crawl_weibo_datas
 import time
-import datetime
-from logger import crawler
 
-STARTTIME = '2019-09-08 11:50:00'
-ENDTIME = '2019-09-10 22:30:00'
-KEYWORD = '#上长下短穿搭法#'
+# 情感榜 https://v6.bang.weibo.com/czv/qinggan?period=month
+uids = [2678630280]
 
-start_time = datetime.datetime.strptime(STARTTIME, "%Y-%m-%d %H:%M:%S")
-end_time = datetime.datetime.strptime(ENDTIME, "%Y-%m-%d %H:%M:%S")
+for uid in uids:
+    crawl_person_infos(uid)
 
-OneHour = datetime.timedelta(hours=1)
-
-time1 = start_time
-time2 = start_time + OneHour
-
-
-def get_topic_data(keyword, start_time='', end_time=''):
-    from tasks.topic import search_keyword_topic
-    search_keyword_topic(keyword, 33, start_time, end_time)
-
-
-while 1:
-    while time1 < end_time:
-        start_time_str = time1.strftime("%Y-%m-%d-%H")
-        end_time_str = time2.strftime("%Y-%m-%d-%H")
-        print(start_time_str, '--', end_time_str)
-
-        crawler.info("we are crawling keyword:{}, timerange {}:{} content" . format(KEYWORD, start_time_str, end_time_str))
-        get_topic_data(KEYWORD, start_time_str, end_time_str)
-        time1 = time1 + OneHour
-        time2 = time2 + OneHour
-
-    time1 = start_time
-    time2 = start_time + OneHour
-
+    crawl_weibo_datas(uid)
+    time.sleep(11)
