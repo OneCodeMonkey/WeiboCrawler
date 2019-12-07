7c0585e9b6af9019337b4f99725bfef8ed57d514
OneCodeMonkey <1460018362@qq.com>
2019-12-07 17:47:47 +0800

    fix

--- tasks/comment.py
+++ tasks/comment.py
@@ -35,7 +35,7 @@ def crawl_comment_by_page(mid, page_num):
 @app.task(ignore_result=True)
 def crawl_comment_page(mid):
     limit = conf.get_max_comment_page() + 1
-    # 这里为了马上拿到返回结果，采用本地调用的方式
+
     first_page = crawl_comment_by_page(mid, 1)[0]
     total_page = comment.get_total_page(first_page)
 
