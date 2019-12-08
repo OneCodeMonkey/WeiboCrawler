098d6cda54def13ddedd2b6cbe8e74ae895a4541
OneCodeMonkey <1460018362@qq.com>
2019-12-08 16:20:00 +0800

    add task

---
+++ node_tasks/group_goodnight_sms/node20_12_task.py
@@ -0,0 +1,39 @@
+# -*- coding=UTF-8 -*-
+import requests
+import time
+import datetime
+from logger import crawler
+
+STARTTIME = '2019-12-06 23:00:00'
+ENDTIME = '2019-12-07 00:00:00'
+KEYWORD = '#晚安短信计划#'
+
+start_time = datetime.datetime.strptime(STARTTIME, "%Y-%m-%d %H:%M:%S")
+end_time = datetime.datetime.strptime(ENDTIME, "%Y-%m-%d %H:%M:%S")
+
+OneHour = datetime.timedelta(hours=1)
+
+time1 = start_time
+time2 = start_time + OneHour
+
+
+def get_topic_data(keyword, start_time='', end_time=''):
+    from tasks.topic import search_keyword_topic
+    search_keyword_topic(keyword, 33, start_time, end_time)
+
+
+# while 1:
+while time1 < end_time:
+    start_time_str = time1.strftime("%Y-%m-%d-%H")
+    end_time_str = time2.strftime("%Y-%m-%d-%H")
+    print(start_time_str, '--', end_time_str)
+
+    crawler.info("we are crawling keyword:{}, timerange {}:{} content" . format(KEYWORD, start_time_str, end_time_str))
+    get_topic_data(KEYWORD, start_time_str, end_time_str)
+    time1 = time1 + OneHour
+    time2 = time2 + OneHour
+
+    # time1 = start_time
+    # time2 = start_time + OneHour
+    # end_time = datetime.datetime.strptime(time.strftime('%Y-%m-%d',time.localtime(time.time())) + ' 00:00:00', "%Y-%m-%d %H:%M:%S")
+
