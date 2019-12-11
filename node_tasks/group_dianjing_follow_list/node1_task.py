a54c449e23439378c9225ebe135802697bf63157
OneCodeMonkey <1460018362@qq.com>
2019-12-12 02:07:31 +0800

    modify

--- node_tasks/group_dianjing_follow_list/node1_task.py
+++ node_tasks/group_dianjing_follow_list/node1_task.py
@@ -1,5 +1,7 @@
 from tasks.workers import app
 from page_get.user import get_fans_or_followers_ids
+from tasks.user import crawl_person_infos
+
 
 uids = [1971239183]
 
@@ -7,5 +9,4 @@ uids = [1971239183]
 for uid in uids:
     result = get_fans_or_followers_ids(uid,2,1)
     for user_id in result:
-        app.send_task('tasks.user.crawl_person_infos', args=(user_id,), queue='user_crawler',
-                      routing_key='for_user_info')
+        crawl_person_infos(user_id)
