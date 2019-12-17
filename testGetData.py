5c71fbd777bc0a5141df6f70b7b6d1c81f92fd4e
OneCodeMonkey <1460018362@qq.com>
2019-12-17 14:20:26 +0800

    test

--- testGetData.py
+++ testGetData.py
@@ -1,12 +1,12 @@
 from tasks.user import crawl_person_infos
 from tasks.home import crawl_weibo_datas
+from tasks.comment import crawl_comment_page
 import time
 
 # 情感榜 https://v6.bang.weibo.com/czv/qinggan?period=month
-uids = [2678630280]
+mids = [4430578961241391]
 
-for uid in uids:
-    crawl_person_infos(uid)
+for mid in mids:
+    crawl_comment_page(mid)
 
-    crawl_weibo_datas(uid)
     time.sleep(11)
