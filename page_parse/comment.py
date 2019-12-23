e2983a21274ab03fdde2384c2b61030e0aca4fdb
OneCodeMonkey <1460018362@qq.com>
2019-12-23 11:57:48 +0800

    test

--- page_parse/comment.py
+++ page_parse/comment.py
@@ -169,8 +169,7 @@ def get_comment_list(html, wb_id):
             crawler.error('解析评论失败，具体信息是{}'.format(e))
         else:
             crawler.info("----28282828----")
-            for i in wb_comment:
-                crawler(str(i) + "--wb_comment item--")
+            crawler("--wb_comment item--")
             comment_list.append(wb_comment)
             crawler.info("----29292929----")
 
