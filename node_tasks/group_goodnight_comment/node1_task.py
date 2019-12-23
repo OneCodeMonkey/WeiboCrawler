b65504b41a409d3b62d46120c8ca56a9af0171e5
OneCodeMonkey <1460018362@qq.com>
2019-12-23 13:43:28 +0800

    modify

--- node_tasks/group_goodnight_comment/node1_task.py
+++ node_tasks/group_goodnight_comment/node1_task.py
@@ -1,5 +1,6 @@
 # -*- coding=UTF-8 -*-
 import time
+import random
 from tasks.comment import crawl_comment_page
 from db.models import WeiboData
 from db.basic import db_session
@@ -9,4 +10,6 @@ from logger import crawler
 mids = db_session.query(WeiboData).filter(WeiboData.weibo_cont.like("%晚安短信计划%")).limit(10).all()
 
 for mid in mids:
-    crawler.info(mid.weibo_id + "--item")
+    crawler.info("crawling comment of weiboid:" + mid.weibo_id)
+    crawl_comment_page(mid)
+    time.sleep(random.randint(1, 4))
