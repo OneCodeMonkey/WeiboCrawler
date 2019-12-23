7949d144f293e81f5dd7b1045b9fe6d4c65c7dda
OneCodeMonkey <1460018362@qq.com>
2019-12-23 13:32:23 +0800

    test

---
+++ node_tasks/group_goodnight_comment/node1_task.py
@@ -0,0 +1,12 @@
+# -*- coding=UTF-8 -*-
+import time
+from tasks.comment import crawl_comment_page
+from db.models import WeiboData
+from db.basic import db_session
+from logger import crawler
+
+
+mids = db_session.query(WeiboData).filter(WeiboData.weibo_cont.like("%晚安短信计划%")).all()
+
+for mid in mids:
+    crawler.info(str(mid) + "--item")
