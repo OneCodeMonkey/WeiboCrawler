7962aeb57074b829fbf04de02cbb1f7659427449
OneCodeMonkey <1460018362@qq.com>
2020-02-05 17:40:11 +0800

    restore

--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,6 @@
 import time
 import datetime
 
-from sqlalchemy import text
-from sqlalchemy.exc import IntegrityError as SqlalchemyIntegrityError
-from pymysql.err import IntegrityError as PymysqlIntegrityError
-from sqlalchemy.exc import InvalidRequestError
+time1 = datetime.datetime.today().strftime("%Y-%m-%d %H:%M")
+print(time1)
 
-from db.basic import db_session
-from db.models import (
-    LoginInfo, KeywordsWbdata, KeyWords, SeedIds, UserRelation,
-    WeiboComment, WeiboRepost, User, WeiboData, WeiboPraise
-)
-
-db_session.query(WeiboData.uid). \
-            filter(text('id>17319011')).all()
