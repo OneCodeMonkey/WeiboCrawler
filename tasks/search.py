319e4d9e189b016845d2c69f0952269f044dfbdd
OneCodeMonkey <1460018362@qq.com>
2020-03-27 14:49:26 +0800

    fix

--- tasks/search.py
+++ tasks/search.py
@@ -29,7 +29,7 @@ def search_keyword(keyword, keyword_id):
     while cur_page < LIMIT:
         cur_url = URL.format(encode_keyword, cur_page)
         # current only for login, maybe later crawling page one without login
-        search_page = get_page(cur_url, auth_level=2)
+        search_page = get_page(cur_url, auth_level=2, need_proxy=True)
         if not search_page:
             crawler.warning('No search result for keyword {}, the source page is {}'.format(keyword, search_page))
             return
